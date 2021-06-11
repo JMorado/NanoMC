b4644c5916313732f1764b5f2b991089cc8cf969
Joao Morado <joaomorado@gmail.com>
2021-06-11 10:34:16 +0100

    LJ bulk fluid and fluid-solid tail corrections

--- src/energy_module.f90
+++ src/energy_module.f90
@@ -1,4 +1,5 @@
 MODULE energy_module
+    USE constants_module
     USE pbc_module
     USE simulation_module
     IMPLICIT NONE
@@ -29,17 +30,94 @@ CONTAINS
         RETURN
     END FUNCTION lennard_jones_energy
 
-    FUNCTION total_system_energy(simulation_instance)
+    FUNCTION lennard_jones_energy_tail_bulk_fluid(rcutoff, epsilon, sigma, density)
+        !========================================================================!
+        ! This function calculates the Lennard-Jones 12-6 energy tail correction !
+        !                                                                        !
+        ! Reference:                                                             !
+        ! Understanding Molecular Simulation                                     !
+        ! Smit B and Frenkel D                                                   !
+        ! Elsevier, 2002. https://doi.org/10.1016/B978-0-12-267351-1.X5000-7.    !
+        ! pp. 36-37                                                              !
+        !------------------------------------------------------------------------!
+        !------------------------------------------------------------------------!
+        ! rcutoff      (in) : cutoff distance                                    !
+        ! epsilon      (in) : potential well depth                               !
+        ! sigma        (in) : distance at which the potential is zero            !
+        ! density      (in) : density                                            !
+        !========================================================================!
+        REAL(8),    INTENT(IN) :: rcutoff, epsilon, sigma
+        REAL(8)                :: lennard_jones_energy
+        REAL(8)                :: rep_term, attract_term, sigma3 ,rcutoff3
+
+        ! Squared values
+        rcutoff3 = rcutoff*rcutoff
+        sigma3 = sigma*sigma
+
+        ! ((sigma/rcut)**9-(sigma/rcut)**3)
+        attract_term = (sigma3 / rcutoff3)
+        rep_term = (sigma3 / rcutoff3)*(sigma3 / rcutoff3)*(sigma3 / rcutoff3)
+
+        ! Lennard-Jones 12-6 energy tail correction
+        lennard_jones_energy = (8.0d0/3.0d0) * density * pi * epsilon * sigma3 * ((1.0d0/3.0d0)*rep_term - attract_term)
+
+        RETURN
+    END FUNCTION lennard_jones_energy_tail_bulk_fluid
+
+    FUNCTION lennard_jones_energy_tail_fluid_solid(rcutoff, epsilon, sigma, density)
+        !========================================================================!
+        ! This function calculates the Lennard-Jones 12-6 energy tail correction !
+        !                                                                        !
+        ! Reference:                                                             !
+        ! Siperstein, F.; Myers, A. L.; Talu, O.                                 !
+        ! Long Range Corrections for Computer Simulations of Adsorption.         !
+        ! Molecular Physics 2002, 100 (13), 2025–2030.                           !
+        ! https://doi.org/10.1080/00268970110109916.                             !                                 !
+        !------------------------------------------------------------------------!
+        ! rcutoff      (in) : cutoff distance                                    !
+        ! epsilon      (in) : potential well depth                               !
+        ! sigma        (in) : distance at which the potential is zero            !
+        ! density      (in) : density of solid atoms                             !
+        !========================================================================!
+        REAL(8),    INTENT(IN) :: rcutoff, epsilon, sigma
+        REAL(8)                :: lennard_jones_energy
+        REAL(8)                :: rep_term, attract_term, sigma3 ,rcutoff3
+
+        ! Squared values
+        rcutoff3 = rcutoff*rcutoff
+        sigma3 = sigma*sigma
+
+        ! ((sigma/rcut)**9-(sigma/rcut)**3)
+        attract_term = (sigma3 / rcutoff3)
+        rep_term = (sigma3 / rcutoff3)*(sigma3 / rcutoff3)*(sigma3 / rcutoff3)
+
+        ! Lennard-Jones 12-6 energy tail correction
+        ! Similar to bulk fluid correction but without the 1/2 factor that corrects for duplicate counting of
+        ! pairwise interactioins in bulk fluids
+        lennard_jones_energy = (16.0d0/3.0d0) * density * pi * epsilon * sigma3 * ((1.0d0/3.0d0)*rep_term - attract_term)
+
+        RETURN
+    END FUNCTION lennard_jones_energy_tail_fluid_solid
+
+    FUNCTION total_system_energy(simulation_instance, energy_tail_correction)
         !========================================================================!
         ! This function calculates the total Lennard-Jones 12-6 energy           !
         ! for a atomistic nanotube representation                                !
+        !                                                                        !
+        ! energy_tail_correction is True if not present                          !
         !------------------------------------------------------------------------!
-        ! simulation_instance (in) : Simulation instance                         !
+        ! simulation_instance    (in) : Simulation instance                      !
+        ! energy_tail_correction (in) : flag to LJ energy tail correction        !
         !========================================================================!
         IMPLICIT NONE
-        TYPE(Simulation),  INTENT(IN) :: simulation_instance
-        REAL(8)                       :: total_system_energy
-        INTEGER(4)                    :: k
+        TYPE(Simulation),     INTENT(IN) :: simulation_instance
+        LOGICAL(1), OPTIONAL, INTENT(IN) :: energy_tail_correction
+        REAL(8)                          :: total_system_energy
+        INTEGER(4)                       :: k
+
+        IF (.not. PRESENT(energy_tail_correction)) THEN
+            energy_tail_correction = .true.
+        END IF
 
         ! Total system energy is equal to sum of invidual energy particle contributions divided by 2
         total_system_energy = 0
@@ -47,6 +125,22 @@ CONTAINS
             total_system_energy = total_system_energy &
                     + particle_energy_atomistic(simulation_instance, k, simulation_instance%coord(:,k)) / 2.0d0
         END DO
+
+
+        ! Add Lennard-Jones 12-6 Tail correction
+        IF (energy_tail_correction .eqv. .True.) THEN
+            ! Add bulk fluid correction
+            total_system_energy = total_system_energy &
+                    + simulation_instance%npart &
+                    * lennard_jones_energy_tail_fluid_solid(simulation_instance%fluid_cut_off, &
+                                    simulation_instance%eps_fluid, simulation_instance%sigma_fluid,simulation_instance%density_fluid)
+            ! Add fluid-solid correction
+            total_system_energy = total_system_energy &
+                    + simulation_instance%ncnt &
+                            * lennard_jones_energy_tail_fluid_solid(simulation_instance%fluid_cut_off, &
+                                    simulation_instance%eps_cnt, simulation_instance%sigma_cnt,simulation_instance%density_cnt)
+        END IF
+
 
         RETURN
     END FUNCTION total_system_energy
